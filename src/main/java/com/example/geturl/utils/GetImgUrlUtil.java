ffcca251ab180ff51cfc4145eea0eb56befc216c
Ash <yukiloh001@gmail.com>
2020-04-20 12:51:25 +0800

    update

--- src/main/java/com/example/geturl/utils/GetImgUrlUtil.java
+++ src/main/java/com/example/geturl/utils/GetImgUrlUtil.java
@@ -14,20 +14,24 @@ public class GetImgUrlUtil {
     public static void getImgUrlUtil(String path) {
         File root = new File(path);
 
-        //guava利用guava的深度优先+先序遍历
+        //利用guava的深度优先+先序遍历
         Traverser<File> fileTraverser = Files.fileTraverser();
         Iterable<File> files = fileTraverser.depthFirstPreOrder(root);
+
+        //正则匹配,参考地址:https://upload-images.jianshu.io/upload_images/5858534-c171c323f995413f.png
         Pattern pattern = Pattern.compile("(https://.*(\\.png)||(\\.jpg))");
 
-        //通过获取的迭代来遍历,利用Consumer来制定文件筛选规则
+        //进行文件夹的遍历
         files.forEach(file -> {
             if (file.isFile()) {
                 try {
-                    List<String> list = Files.readLines(file, Charsets.UTF_8);
-                    //参考地址:https://upload-images.jianshu.io/upload_images/5858534-c171c323f995413f.png
-                    list.forEach(line->{
+                    List<String> post = Files.readLines(file, Charsets.UTF_8);
+
+                    //遍历单篇文章的每一行
+                    post.forEach(line->{
                         Matcher matcher = pattern.matcher(line);
                         while (matcher.find()) {
+                            //匹配则打印结果
                             if (!matcher.group().isEmpty()) System.out.println(matcher.group());
                         }
                     });
